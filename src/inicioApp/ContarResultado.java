6f13303cf4aa84ff5baea5fe4677e9cf40bf8078
Roberto <Roberto@LAPTOP-LCKB6K89>
2020-05-09 14:32:39 +0200

    Este commit lo he realizado para guardar el javadoc de las clases siguientes: - ContarResultado.java - PintarResultado.java - inicioApp.java

--- src/inicioApp/ContarResultado.java
+++ src/inicioApp/ContarResultado.java
@@ -1,16 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inicioApp;
 
-/**
- *
+/*
+ * Esta clase contiene todas las acciones que se necesitan para poder contar las palabras, caracteres y 
+ * cuantos caracteres se repiten.
  * @author Roberto
+ * @version 1.0
  */
 public class ContarResultado {
 
+/*
+ * Este metodo lo que hace sera contar cuantos caracteres hay en total en toda la cadena que le hemos
+ * indicado en el metodo main.
+ * @param Cadena
+ * @return ContadorCaracteres
+*/
     static int contarCaracteresTotal(String Cadena) {
         for (int i = 0; i < Cadena.length(); i++) {
             if (Cadena.charAt(i) != ' ') {
@@ -19,7 +22,13 @@ public class ContarResultado {
         }
         return inicioApp.ContadorCaracteres;
     }
-
+/*
+ * En este metodo lo que haremos sera contar los espacios que tenemos en la cadena de texto pasada en 
+ * el metodo main. Este metodo lo podremos incluir en el metodo anterior, pero se puede dejar asi para
+ * poder diferenciarlo.
+ * @param Cadena
+ * @return ContadorEspacios
+*/
     static int contarNumeroPalabras(String Cadena) {
         for (int i = 0; i < Cadena.length(); i++) {
             if (Cadena.charAt(i) == ' ') {
@@ -29,6 +38,11 @@ public class ContarResultado {
         return inicioApp.ContadorEspacios;
     }
 
+/*
+ * Este metodo lo que hara sera contar los caracteres que se repiten, y los guardara en un array de int.
+ * @param Cadena
+ * @return contadorVecesRepetida
+*/
     static int[] contarTodosCaracteres(String Cadena) {
         for (int i = 0; i < Cadena.length(); i++) {
             char letraActual = Cadena.charAt(i);
--- src/inicioApp/PintarResultado.java
+++ src/inicioApp/PintarResultado.java
@@ -1,24 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inicioApp;
 
-/**
- *
+/*
+ * Esta clase lo que hara sera pasar al metodo main lo que tiene que escribir por pantalla.
  * @author Roberto
+ * @version 1.0
  */
 public class PintarResultado {
 
+/*
+ * Este metodo lo que hara sera pintar la frase de cuantos caracteres hay en total.
+ * @param ContadorCaracteres
+ * @return "Hay un total de " + ContadorCaracteres + " caracteres."
+*/
     static String textoCaracterTotal(int ContadorCaracteres) {
         return "Hay un total de " + ContadorCaracteres + " caracteres.";
     }
-
+    
+/*
+ * Este metodo lo que hara sera pintar la frase de cuantos espacios hay en total.
+ * @param ContadorEspacios
+ * @return "Hay un total de " + ContadorEspacios + " palabras"
+*/
     static String textoNumeroPalabras(int ContadorEspacios) {
         return "Hay un total de " + ContadorEspacios + " palabras";
     }
 
+/*
+ * Este metodo lo que hara sera pintar las veces que se repite cada letra hasta el final de la frase.
+ * @param i
+ * @return "La letra " + arrayLetrasRepetidas[i] + " se repite " + contadorVecesRepetida[i]
+*/
     static String textoTodosCaracteres(int i) {
         return "La letra " + inicioApp.arrayLetrasRepetidas[i] + " se repite " + inicioApp.contadorVecesRepetida[i];
     }
